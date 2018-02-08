e9cdde7c211de742cb1c9e899de77dd7247cc19f
AhmedHafez98 <ah.hafez1098@gmail.com>
2018-02-08 18:45:50 +0200

    HackerEarth/9.cpp

---
+++ HackerEarth/9.cpp
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v) ((v).begin()),((v).end())
+#define allr(v)      ((v).rbegin()), ((v).rend())
+#define sz(v) ((int)(v).size())
+#define clr(v, d)        memset(v, d, sizeof(v))
+#define JSE ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl  "\n"
+typedef long long ll;
+typedef unsigned long long ull;
+ll mod = ll(1e9 + 7);
+typedef  double ld;
+void file(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin); //freopen("out.txt", "w", stdout);
+#endif
+}
+int arr[3];
+int main()
+{
+	//cout << setprecision(7) << fixed;
+	JSE
+		//		file();
+		//  freopen("friday.in", "r", stdin);
+		///////////
+		string s, a;
+	cin >> s;
+	a = "love";
+	int idx = 0;
+	for (int i = 0; i < sz(s); i++)
+	{
+		if (s[i] == a[idx])
+			idx++;
+		if (idx == 4)
+			break;
+	}
+	if (idx == 4)
+		cout << "I love you, too!" << endl;
+	else
+		cout << "Let us breakup!" << endl;
+}
