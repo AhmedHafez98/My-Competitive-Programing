bcd05c93ca0bbfa30f6996101fa0aab24d698cdc
AhmedHafez98 <ah.hafez1098@gmail.com>
2018-02-08 17:59:29 +0200

    Codeforces/2.cpp

---
+++ Codeforces/2.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v) ((v).begin()),((v).end())
+#define allr(v)	     ((v).rbegin()), ((v).rend())
+#define sz(v) ((int)(v).size())
+#define clr(v, d)        memset(v, d, sizeof(v))
+#define JSE ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl  "\n"
+typedef long long ll;
+typedef unsigned long long ull;
+ll mod = ll(1e9 + 7);
+typedef  double ld;
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+void file(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin); freopen("out.txt", "w", stdout);
+#endif
+}
+int b[6];
+int main()
+{
+	//cout << setprecision(4) << fixed;
+	JSE
+		//file();
+		//	freopen("friday.in", "r", stdin);
+		///////////
+		int n;
+	cin >> n;
+	while (n--)
+	{
+		int a, b;
+		cin >> a >> b;
+		if (a >= b)
+			cout << "FunkyMonkeys" << endl;
+		else
+			cout << "WeWillEatYou" << endl;
+	}
+}
