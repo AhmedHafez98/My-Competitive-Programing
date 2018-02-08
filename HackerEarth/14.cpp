91ad3a78104666947bbfaac42a1163d0b396647b
AhmedHafez98 <ah.hafez1098@gmail.com>
2018-02-08 18:56:11 +0200

    HackerEarth/14.cpp

---
+++ HackerEarth/14.cpp
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v) ((v).begin()),((v).end())
+#define allr(v)      ((v).rbegin()), ((v).rend())
+#define sz(v) ((int)(v).size())
+#define clr(v, d)        memset(v, d, sizeof(v))
+#define JSE ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl  "\n"
+typedef long long ll;
+typedef unsigned long long ull;
+ll mod = ll(1e9 + 7);
+typedef  double ld;
+void file(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin); //freopen("out.txt", "w", stdout);
+#endif
+}
+int main()
+{
+	//cout << setprecision(7) << fixed;
+	JSE
+		//	file();
+		//  freopen("friday.in", "r", stdin);
+		int t;
+	cin >> t;
+	while (t--)
+	{
+		string s;
+		cin >> s;
+		set<char>st;
+		for (int i = 0; i < sz(s); i++)
+			st.insert(s[i]);
+		if (sz(st) < 26)
+			cout << "NO" << endl;
+		else
+			cout << "YES" << endl;
+	}
+}
