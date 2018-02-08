795d09b864e5f6235519b65ee18aad7109b70d05
AhmedHafez98 <ah.hafez1098@gmail.com>
2018-02-08 17:51:02 +0200

    HackerRank/3.cpp

---
+++ HackerRank/3.cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v) ((v).begin()),((v).end())
+#define allr(v)	     ((v).rbegin()), ((v).rend())
+#define sz(v) ((int)(v).size())
+#define clr(v, d)        memset(v, d, sizeof(v))
+#define JSE ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl  "\n"
+typedef long long ll;
+typedef unsigned long long ull;
+ll mod = ll(1e9 + 7);
+typedef  double ld;
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+void file(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin); freopen("out.txt", "w", stdout);
+#endif
+}
+
+int main()
+{
+	//cout << setprecision(4) << fixed;
+	JSE
+		//file();
+		//	freopen("friday.in", "r", stdin);
+		///////////
+		int t;
+	cin >> t;
+	int idx = 1;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+
+		ll x;
+		ll ans = 1;
+		for (int i = 0; i < n - 1; i++)
+		{
+			cin >> x;
+			ans *= x;
+			ans %= 1234567;
+		}
+		cout << ans << endl;
+	}
+
+}
