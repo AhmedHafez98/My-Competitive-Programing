36a0f1ddf65685c6dbf3316f3107c5744e9850b1
AhmedHafez98 <ah.hafez1098@gmail.com>
2018-02-08 19:20:27 +0200

    Codeforces/18.cpp

---
+++ Codeforces/18.cpp
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v) ((v).begin()),((v).end())
+#define allr(v)      ((v).rbegin()), ((v).rend())
+#define sz(v) ((int)(v).size())
+#define clr(v, d)        memset(v, d, sizeof(v))
+#define JSE ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl  "\n"
+typedef long long ll;
+typedef unsigned long long ull;
+ll mod = ll(1e9 + 7);
+typedef  double ld;
+void file(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin); //freopen("out.txt", "w", stdout);
+#endif
+}
+bool a[10000001];
+int main()
+{
+	//cout << setprecision(7) << fixed;
+	JSE
+		//		file();
+		//  freopen("friday.in", "r", stdin);
+		///////////
+		ll n, k;
+	cin >> n >> k;
+	bool f = 1;
+	for (int i = 1; i <= min(k, 10000001LL); i++)
+	{
+		if (a[n%i])
+			f = 0;
+		else
+			a[n%i] = 1;
+	}
+	cout << (f ? "Yes" : "No") << endl;
+
+}
