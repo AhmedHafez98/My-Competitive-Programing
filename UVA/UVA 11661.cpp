dad8e6444f25c7b8eca0519a0b1b9cc8d6be9ca9
AhmedMohamed98 <ah.hafez1098@gmail.com>
2018-03-02 11:24:24 +0200

    add

---
+++ UVA/UVA 11661.cpp
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) ((v).begin()),((v).end())
+#define allr(v)	     ((v).rbegin()), ((v).rend())
+#define sz(v) ((int)(v).size())
+#define clr(arr, val)        memset(arr, val, sizeof(arr))
+#define JSE ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MP(i,j) make_pair(int i,int j);
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef  double ld;
+
+const ll OO = (ll)1e18;
+const ll mod = (1e9) + 7;
+const ld PI = acos(-1.0);
+const ld EPS = 1e-8;
+
+int dx[]{-1, -1, -1, 0, 0, 1, 1, 1}; // Dir array
+int dy[]{-1, 0, 1, -1, 1, -1, 0, 1}; // Dir array
+
+void file(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);  
+#endif
+}
+int a[int(1e5)];
+int main()
+{
+	//cout << setprecision(20) << fixed;
+	// negative MOD (a%x+x)%x;
+	file();
+	//	freopen("friday.in", "r", stdin);
+	///////////
+	JSE
+		int n;
+	while (cin >> n)
+	{
+		if (!n)
+			break;
+		string s;
+		cin >> s;
+		int d = -1, r = -1, z, mn = INT_MAX;
+		for (int i = 0; i < n; i++)
+		{
+			if (s[i] == 'D')
+			{
+				d = i;
+				if (r!=-1)
+				mn = min(mn, abs(d - r));
+			}
+			else if (s[i] == 'R')
+			{
+				r = i;
+				if (d!=-1)
+				mn = min(mn, abs(d - r));
+			}
+			else if (s[i] == 'Z')
+			{
+				mn = 0; break;
+			}
+		}
+		cout << mn << endl;
+	}
+	return 0;
+}
