8b76730af47afa4ba0e85588e8bd91f11304ca8e
AhmedMohamed98 <ah.hafez1098@gmail.com>
2018-03-28 20:12:55 +0200

    add

---
+++ Codeforces/CF68-D2-B.cpp
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) ((v).begin()),((v).end())
+#define allr(v)	     ((v).rbegin()), ((v).rend())
+#define sz(v) ((int)(v).size())
+#define clr(arr, val)        memset(arr, val, sizeof(arr))
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef  double ld;
+
+const ll OO = (ll)1e18;
+const ll mod = (1e9) + 7;
+const ld PI = acos(-1.0);
+const ld EPS = 1e-8;
+
+int dx[]{-1, -1, -1, 0, 0, 1, 1, 1}; // Dir array
+int dy[]{-1, 0, 1, -1, 1, -1, 0, 1}; // Dir array
+
+void file(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+
+	//freopen("output.txt", "w", stdout);
+
+#endif
+}
+int f(vector<ll>&v, ll mid, ll m)
+{
+	ll up = 0, down = 0;
+	for (int i = 0; i < sz(v); i++)
+	{
+		if (v[i]>mid)
+			up += v[i] - mid;
+		else
+			down += mid - v[i];
+	}
+	ll ans = up*m;
+	ans /= 100;
+	up -= ans;
+	if (up >= down)
+		return 1;
+	else if (up < down)
+		return 0;
+}
+int main()
+{
+	cout << setprecision(7) << fixed;
+	// negative MOD (a%x+x)%x;
+
+	//	freopen("friday.in", "r", stdin);
+	///////////
+	file();
+
+	fast
+		int n, m;
+	cin >> n >> m;
+	vector<ll>v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+		v[i] *= 1e7;
+	}
+	ll lo = 0, hi = 1e10, ans = 0;
+	while (lo <= hi)
+	{
+		ll mid = (lo + hi) / 2;
+		int a = f(v, mid, m);
+		if (a)
+		{
+			ans = mid;
+			lo = mid + 1;
+		}
+		else if (a==0)
+		{
+			hi = mid - 1;
+		}
+	}
+	double an = ans / 10000000.0;
+	cout << an << endl;
+	return 0;
+}
